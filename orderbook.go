da7b3121d5f3014b5fc6eab9258001cdf0e7c9f8
Hossein <hshafiei374@gmail.com>
2024-07-18 18:29:29 +0330

    resort the orders

--- orderbook.go
+++ orderbook.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"time"
 )
 
@@ -19,6 +20,12 @@ type Order struct {
 	Timestamp int64
 }
 
+type Orders []*Order
+
+func (o Orders) Len() int           { return len(o) }
+func (o Orders) Swap(i, j int)      { o[i], o[j] = o[j], o[i] }
+func (o Orders) Less(i, j int) bool { return o[i].Timestamp < o[j].Timestamp }
+
 func NewOrder(bid bool, size float64) *Order {
 	return &Order{
 		Bid:       bid,
@@ -34,9 +41,23 @@ func (o *Order) String() string {
 // a group of orders at the certain price level
 type Limit struct {
 	Price       float64
-	Orders      []*Order
+	Orders      Orders
 	TotalVolume float64
 }
+
+type Limits []*Limit
+
+type ByBestAsk struct{ Limits }
+
+func (a ByBestAsk) Len() int           { return len(a.Limits) }
+func (a ByBestAsk) Swap(i, j int)      { a.Limits[i], a.Limits[j] = a.Limits[j], a.Limits[i] }
+func (a ByBestAsk) Less(i, j int) bool { return a.Limits[i].Price < a.Limits[j].Price }
+
+type ByBestBid struct{ Limits }
+
+func (b ByBestBid) Len() int           { return len(b.Limits) }
+func (b ByBestBid) Swap(i, j int)      { b.Limits[i], b.Limits[j] = b.Limits[j], b.Limits[i] }
+func (b ByBestBid) Less(i, j int) bool { return b.Limits[i].Price < b.Limits[j].Price }
 
 func NewLimit(price float64) *Limit {
 	return &Limit{
@@ -61,7 +82,8 @@ func (l *Limit) DeleteOrder(o *Order) {
 		}
 	}
 
-	//TODO: resort the whole orders
+	//resort the whole orders
+	sort.Sort(l.Orders)
 
 }
 
